42c64b51875d20d89e7a9bb5fa98c9f7911f4028
Erik Flores <erikfloresper@outlook.com>
2024-03-15 17:44:33 -0600

    first commit

---
+++ src/components/Clima.vue
@@ -0,0 +1,22 @@
+<script setup>
+import useclima from '../composables/useClima';
+    const {formatearTemp} = useclima()
+
+    defineProps({
+        clima:{
+            type:Object,
+            required:true
+        }
+    })
+</script>
+
+<template>
+    <div class="resultado">
+        <h2>Clima de:{{ clima.name }}</h2>
+        <p class="actual">&deg;C{{ formatearTemp(clima.main.temp) }}</p>
+        <div class="temperaturas">
+            <p>Min: {{ formatearTemp(clima.main.temp_min) }}<span>&deg;C</span></p>
+            <p>Max: {{ formatearTemp(clima.main.temp_max) }}<span>&deg;C</span></p>
+        </div>
+    </div>
+</template>
